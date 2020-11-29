f90fac9f11300717c870d792028cf43fe624f867
Jatin Dogra <dogra.jatin5@gmail.com>
2020-11-29 23:42:59 +0530

    Update onExecute.sh

--- steps/playbook/ansible/onExecute.sh
+++ steps/playbook/ansible/onExecute.sh
@@ -2,7 +2,7 @@ execute_step() {
   local gitRepoName=$(find_step_configuration_value "gitRepoName")
   echo $gitRepoName
   pwd
-  ls
+  ls /var/opt/jfrog/pipelines/data/extensions_pipelines/runs/1990/steps/extension_step/3071/dependencyState/resources/extension_resource
   ansible-playbook ansible.yml
 }
 execute_command execute_step
