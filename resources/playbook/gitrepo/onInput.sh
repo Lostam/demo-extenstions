498a89fd44cc956e5a31d612a65c8079561b2df0
Jatin Dogra <dogra.jatin5@gmail.com>
2020-11-29 15:15:55 +0530

    Update onInput.sh

--- resources/playbook/gitrepo/onInput.sh
+++ resources/playbook/gitrepo/onInput.sh
@@ -1,9 +1,6 @@
 execute_input() {
   rm -rf $res_extension_resource_gitRepoName
   git clone --branch $res_extension_resource_gitBranch $res_extension_resource_gitUrl
-  cd $res_extension_resource_gitRepoName
-  git status
-  ls
 }
  
 execute_command "execute_input %%context.resourceName%%"
