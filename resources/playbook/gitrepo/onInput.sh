24c94ee026bf686434f923cd7c57f365a9ac80a3
Jatin Dogra <dogra.jatin5@gmail.com>
2020-11-29 23:31:19 +0530

    Update onInput.sh

--- resources/playbook/gitrepo/onInput.sh
+++ resources/playbook/gitrepo/onInput.sh
@@ -2,7 +2,6 @@ execute_input() {
   local branch=$(find_resource_variable %%context.resourceName%% gitBranch)
   local url=$(find_resource_variable %%context.resourceName%% gitUrl)
   git clone --branch $branch $url
-  printenv
 }
  
 execute_command "execute_input %%context.resourceName%%"
