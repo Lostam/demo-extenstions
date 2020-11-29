7296e2ae245a17810261da3000eda24cf383ae11
Jatin Dogra <dogra.jatin5@gmail.com>
2020-11-29 23:42:35 +0530

    Update onExecute.sh

--- steps/playbook/ansible/onExecute.sh
+++ steps/playbook/ansible/onExecute.sh
@@ -2,6 +2,7 @@ execute_step() {
   local gitRepoName=$(find_step_configuration_value "gitRepoName")
   echo $gitRepoName
   pwd
+  ls
   ansible-playbook ansible.yml
 }
 execute_command execute_step
