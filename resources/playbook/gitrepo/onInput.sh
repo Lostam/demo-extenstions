626e7867ab3a5c8a0ea60bae4464bcf0163a3847
Jatin Dogra <dogra.jatin5@gmail.com>
2020-11-29 14:50:43 +0530

    Update onInput.sh

--- resources/playbook/gitrepo/onInput.sh
+++ resources/playbook/gitrepo/onInput.sh
@@ -1,5 +1,5 @@
 execute_input() {
-  rm -rf demo-extenstions
+  ls
   echo "git clone $res_extension_resource_gitUrl"
   git clone https://github.com/jatindogra/demo-extenstions.git
   git checkout $res_extension_resource_gitBranch
