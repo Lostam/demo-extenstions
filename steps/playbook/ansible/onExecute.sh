92b367650d968a20ce894d2d5d9dd3e686c4f257
Jatin Dogra <dogra.jatin5@gmail.com>
2020-11-30 00:02:39 +0530

    Update onExecute.sh

--- steps/playbook/ansible/onExecute.sh
+++ steps/playbook/ansible/onExecute.sh
@@ -1,8 +1,8 @@
 execute_step() {
   local resourceName=$(find_step_configuration_value "resourceName")
   local fileName=$(find_step_configuration_value "fileName")
-  local resourcePath=$(find_resource_variable "$resourceName" resourcePath)/$fileName
-  printenv
+  local gitRepoName=$(find_resource_variable $resourceName gitRepoName)
+  local resourcePath=$(find_resource_variable $resourceName resourcePath)/$gitRepoName/$fileName
   echo "Changing directory: $resourcePath"
   pushd $resourcePath
   ansible-playbook ansible.yml
